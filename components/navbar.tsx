fdabe198b021206112eeda115d8d4ff9eeefa8f3
kirito112a <82517939+kirito112a@users.noreply.github.com>
2023-05-25 17:52:54 +0700

    Add files via upload

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -57,8 +57,9 @@ function Navbar() {
         <div className="container">
           <div className="row">
             <ul>
-              <li><a className="active col-12" href="#home">หน้าแรก</a></li>
+            <li><a className="active col-12" href="#home"> <img className="underbar_img  img-fluid" src="/home.png" alt="img1"  ></img>   <br></br> หน้าแรก</a></li>
               <li><a className="col-12" href="#contact">
+              <img className="underbar_img  img-fluid" src="/register.png" alt="img1"  ></img> 
                 <span
                   onClick={openModal_register}>
                   <div id="root"></div>
@@ -84,11 +85,12 @@ function Navbar() {
               </li>
               <li><a className="col-12" href="#news"><span onClick={openModal_signin}>
                 <div id="root"></div>
+                <img className="underbar_img  img-fluid" src="/home.png" alt="img1"  ></img><br></br> 
                 เข้าสู่ระบบ
               </span></a></li>
 
-              <li><a className="col-12" href="#news">โอนย้ายสมาชิก </a></li>
-              <li><a className="col-12" href="#about">ลืมรหัสผ่าน </a></li>
+              <li><a className="col-12" href="#news"><img className="underbar_img  img-fluid" src="/tranfer.png" alt="img1"  ></img><br></br> โอนย้ายสมาชิก </a></li>
+              <li><a className="col-12" href="#about"><img className="underbar_img  img-fluid" src="/forgetPW.png" alt="img1"  ></img><br></br>ลืมรหัสผ่าน </a></li>
             </ul>
           </div>
         </div>
@@ -124,7 +126,7 @@ function Navbar_user()  {
           onClick={openModal_w_t}
         >
           <div id="root"></div>
-          <Link href="/" className="button_nav1" > ประวัติฝาก-ถอน&nbsp;</Link>
+          <Link href="#" className="button_nav1" > ประวัติฝาก-ถอน&nbsp;</Link>
         </span>
         <Modal_Boostrap show={show_w_t} onHide={closeModal_w_t}
 
@@ -148,7 +150,7 @@ function Navbar_user()  {
           onClick={openModal_friend}
         >
           <div id="root"></div>
-          <Link href="/" className="button_nav1" >ชวนเพื่อน&nbsp; </Link>
+          <Link href="#" className="button_nav1" >ชวนเพื่อน&nbsp; </Link>
 
         </span>
 
@@ -175,11 +177,11 @@ function Navbar_user()  {
       <div >
         <span onClick={openModal_profile}>
           <div id="root"></div>
-          <Link href="/" className="button_nav1" >
+          <Link href="#" className="button_nav1" >
             โปรไฟล์ &nbsp;
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16" >
               <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-              <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+              <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
             </svg></Link>
         </span>
 
@@ -200,18 +202,19 @@ function Navbar_user()  {
 
         </Modal_Boostrap>
       </div>
-      <Link href="/"  >
-        <button className="Logout" type="submit">ออกจากระบบ</button></Link>
+      <Link href="/"  > <button className="Logout" type="submit">ออกจากระบบ</button></Link>
 
       <div className="navfooterbar_phone">
         <div className="container">
           <div className="row justify-content-md-center">
             <ul>
-              <li><a className="active col-12" href="#home">หน้าแรก</a></li>
+              <li><a className="active col-12" href="#home"> <img className="underbar_img  img-fluid" src="/home.png" alt="img1"  ></img>   <br></br> หน้าแรก</a></li>
               <li><a className="col-12" >
                 <div >
                   <span onClick={openModal_w_t}>
-                    <div id="root"></div>
+
+                    <img className="underbar_img  img-fluid" src="/wNt.png" alt="img1"  ></img>
+                    <br></br>
                     ประวัติฝาก-ถอน
 
                   </span>
@@ -220,16 +223,22 @@ function Navbar_user()  {
               <li><a className="col-12">   <div >
                 <span onClick={openModal_profile}>
                   <div id="root"></div>
+                  <img className="underbar_img  img-fluid" src="/profile.png" alt="img1"  ></img>
+                  <br></br>
                   โปรไฟล์
                 </span>
 
               </div></a></li>
               <li><a className="col-12" > <span onClick={openModal_friend}>
-                <div id="root"></div>
+                <img className="underbar_img img-fluid" src="/friend.png" alt="img1"></img>
+                <br></br>
+
                 เชิญชวน
               </span>
               </a></li>
-              <li><a className="col-12"   >ออกจากระบบ</a></li>
+
+              <li><Link href={'/'} className="col-12"   >  <img className="underbar_img  img-fluid" src="/m8.png" alt="img1"  ></img>  <br></br>ออกจากระบบ</Link></li>
+
             </ul>
           </div>
         </div>
